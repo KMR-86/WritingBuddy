edf961177ee2cea0984d9d4978267d2cce49d45d
Khandaker Mushfiqur Rahman <mushfiqur.rahman1@booking.com>
2025-03-08 22:09:49 +0100

    Initial Commit

---
+++ src/main/java/com/example/WritingBuddy/WritingBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.WritingBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritingBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritingBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WritingBuddy/WritingBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WritingBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritingBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
